d14052dac77aacb91d6ba69b8b137a4baa06a6aa
ParkMi-Hyun <parkmihyunn@gmail.com>
2022-11-07 02:19:10 +0900

    시작페이지, 푸터
    푸터 하단 고정 시켜야함

--- advent-front/components/footer.js
+++ advent-front/components/footer.js
@@ -1,7 +1,29 @@
 export default function Footer(){
     return (
-        <>
-            <h1>푸터</h1>
-        </>
+        <footer className="text-gray-600 body-font">
+            <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+                <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                    </svg> */}
+                    <span className="text-xl text-gray-500">
+                        백두산호랑팀
+                    </span>
+                </a>
+                <div className="flex flex-row">
+                    <p className="flex flex-row text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+                        © 2022 백두산호랑팀 - all rights reserved
+                    </p>
+                    <span className="flex flex-row text-sm text-gray-500 ml-2 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+                        <a className="text-gray-500 ">
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </footer>
     );
 }
