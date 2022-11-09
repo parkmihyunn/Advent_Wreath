8db7119837fbc081816af3e5f056438c491b3e01
ParkMi-Hyun <parkmihyunn@gmail.com>
2022-11-09 20:25:49 +0900

    main.js
    메인페이지 윗부분 수정

--- advent-front/pages/main.js
+++ advent-front/pages/main.js
@@ -4,9 +4,16 @@ import Layout from '../components/layout'
 import Link from 'next/link'
 
 export default function Main(){
+
+    // 디데이 계산
+    var today = new Date();
+    var dDay = new Date(2022,11,25);
+    var gap = dDay.getTime() - today.getTime();
+    var result = Math.ceil(gap / (1000 * 60 * 60 * 24));
+
     return (
         <div className="
-            flex flex-col h-screen max-w-md
+            flex flex-col items-center h-screen max-w-md
             overflow-auto bg-cover bg-local
             bg-[url('../public/img/wood_pattern.png')]
         ">
@@ -17,50 +24,67 @@ export default function Main(){
             </Head> 
         
             <div className="flex flex-col h-full">
+
                 <h1 className="
                     flex flex-col ml-3 mt-3
                     text-2xl text-left text-white pt-10 px-5
                 ">서비스 이름</h1>
-                <div className="flex mt-4 mb-4 px-10">
-                    <h1 className="flex basis-full
-                        text-xl text-right text-white
-                    ">OOO님의 소원양말</h1>
-                    <Link href='/wish-socks'>
-                    <h1 className="basis-full text-right ml-18 text-gray-500">
-                        편집하기
-                    </h1>
-                    </Link>
-                </div>
-                <div className="px-10">
-                    <div className = "bg-none px-10">
-                         <Image src='/img/socks_line.png' width='350' height='50'/>
-                         <div className="grid grid-rows-1 grid-flow-col gap-5">
-                            <Image src='/img/sock_1.png' width='70' height='120'/>
-                            <Image src='/img/sock_2.png' width='70' height='120'/>
-                            <Image src='/img/sock_3.png' width='70' height='120'/>
+
+                <div className="relative">
+                    <div className="text-center m-auto relative">
+                        <Image src='/img/door-border_1.png' width='307' height='537'/>
+                    </div>
+                    <div className="door-top">
+                        <div className="flex justify-between mt-4 mb-1">
+                            <h1 className="
+                                ml-14 
+                                text-white align-bottom
+                            ">OOO님의 소원양말</h1>
+                            <Link href='/wish-socks'>
+                                <h1 className="mr-14 mt-1 text-gray-500 text-xs align-bottom">
+                                    편집하기
+                                </h1>
+                            </Link>
+                        </div>
+                        <div className="px-10 relative">
+                            <div className="w-60 text-center m-auto relative">
+                                <Image src='/img/socks_line.png' width='350' height='50'/>
+                            </div>
+                            <div className="sock-1">
+                                <Image src='/img/sock_1.png' width='83.95' height='102.5'/>
+                            </div>
+                            <div className="sock-2">
+                                <Image src='/img/sock_2.png' width='79.07' height='109.33p'/>
+                            </div>
+                            <div className="sock-3">
+                                <Image src='/img/sock_3.png' width='82.98' height='107.38'/>
+                            </div>
+                        </div>
+                        <div className="pt-16 mt-6 place-items-center flex-1">
+                            <Image src='/img/wreath_non_2.png' width='280' height='280'/>
+                            <div className="wreath-text">
+                                <Image src='/img/christmas_text.png' width='97' height='25'/>
+                                <h1 className="text-center text-xl">
+                                D-{result}
+                                </h1>
+                            </div>
+                        </div>    
+                        <Link href='/ornaments'>
+                            <h1 className="text-center text-xs text-gray-500">
+                                편집하기
+                            </h1>
+                        </Link>
+                        <div className="flex justify-between px-10" >
+                            <div className="door-handle">
+                                <Image src='/img/handle.png' width='76' height='103'/>
+                            </div>
+                            <div className="collection">
+                                <Image src='/img/collection.png' width='95' height='131'/>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="grid place-items-center flex-1 bg-none relative">
-                    <Image src='/img/wreath_non.png' width='300' height='280'/>
-                    <div className="absolute">
-                        <Image src='/img/christmas_text.png' width='120' height='40'/>
-                        <h1 className="text-center text-2xl">
-                          D-15
-                        </h1>
-                    </div>
-                </div>    
-                <div>
-                <Link href='/ornaments'>
-                    <h1 className="text-center text-base text-gray-500">
-                        편집하기
-                    </h1>
-                </Link>
-                </div>
-                <div className="grid grid-rows-1 grid-flow-col gap-20 px-10" >
-                    <Image src='/img/handle.png' width='40' height='140'/>
-                    <Image src='/img/collection.png' width='80' height='140'/>
-                </div> 
+
                 {/* <div className="px-10 place-items-center grid">
                     <Image src='/img/quiz_deco.png' width='300' height='100'/>
                 </div> */}
