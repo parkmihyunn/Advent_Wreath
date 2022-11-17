eccb051dca081d33bd744c9fa20d3b3ae406921a
ParkMi-Hyun <parkmihyunn@gmail.com>
2022-11-18 02:51:24 +0900

    .
    .

--- advent-front/next.config.js
+++ advent-front/next.config.js
@@ -9,3 +9,14 @@ const nextConfig = {
 
 module.exports = nextConfig
 module.exports = withImages();
+
+module.exports = {
+  reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    };
+    return config;
+  }
+};
