3276055125ac59a0396ed1bac645c68e7cdc5d12
ParkMi-Hyun <parkmihyunn@gmail.com>
2022-11-27 01:30:18 +0900

    .
    .

--- advent-front/pages/quiz.js
+++ advent-front/pages/quiz.js
@@ -37,9 +37,15 @@ export default function quiz(){
     axios.get('http://localhost:3000/api/temp').then(
       res => {
         console.log(res.data);
-        setData(res.data);
+        setData({...res.data[0]});
     });
   },[])
+
+  /*
+  console.log(data);
+  console.log("=================================");
+  console.log((data.quizzes)[0]);
+   */
 
   /* 임시로 사용할 quiz data*/
   const quizData = [
@@ -52,7 +58,7 @@ export default function quiz(){
   ]
 
   /* 문제 가져오기 */
-  //const answerNum = data[0].quizzes[0].quizNum;
+  //const answerNum = data.quizzes[0].quizNum;
   const answerNum = quizData[0].quizNum;
   const answerAsk = quizData[0].question;
   let answerResult = quizData[0].correct.toLowerCase().trim();
