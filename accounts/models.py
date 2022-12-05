0130f7f8986b8a451e8c1e9c0ce3967761de99c1
sehyuni <foxkh24100@hanyang.ac.kr>
2022-12-06 02:44:35 +0900

    유저 모델 수정 및 토큰 저장

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
-from .managers import CustomUserManager
+#from .managers import CustomUserManager
 
 
-class User(AbstractBaseUser):
-    #username = models.CharField(max_length=100, unique=True)
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
     uid = models.CharField(blank=False, null=True, unique=True, max_length=100)
     #user_email = models.EmailField(_('email address'), unique=True, null=True)
-    name = models.CharField(max_length=100,null=True)
+    #name = models.CharField(max_length=100,null=True)
     #gender = models.CharField(max_length=100,null=True)
-    USERNAME_FIELD = 'uid'
+    #USERNAME_FIELD = 'uid'
     #2objects = CustomUserManager()
-    
+    jwt = models.CharField(blank=False, null=True, unique=True,max_length=100)
+
+
     def __str__(self):
         return str(self.uid)
