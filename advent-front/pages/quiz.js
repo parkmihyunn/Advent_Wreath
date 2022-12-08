167a0335f887dae4819f5c43457f9f7f42268f15
ParkMi-Hyun <parkmihyunn@gmail.com>
2022-12-08 23:10:50 +0900

    .
    .

--- advent-front/pages/quiz.js
+++ advent-front/pages/quiz.js
@@ -8,7 +8,51 @@ import ReinModal from "../components/reindeerModal";
 import axios from 'axios';
 
 export default function quiz(){
-  
+  /* quiz data */
+  const quizzes = [
+    {
+      question : '몇몇 순록은 얼음이 아닌 눈에 덮인 이끼를 찾아 남쪽으로 무려 OOOkm까지 이동한다',
+      correct : '100',
+      hints : 'https://www.bbc.com/korean/features-59777860',
+    },{
+        question : '영국 감시단체는 오염으로 OOOOO 가까워진다고 경고하였다',
+        correct : '회복불가능',
+        hints : 'https://www.bbc.com/korean/news-61419228'
+    }, {
+        question : '환경보호 중요성 알리기는 환경보호 실천 방법 리스트 중 몇 번째 목표인가요? ex) 10',
+        correct : '25',
+        hints : 'https://brunch.co.kr/@hantole/13'
+    }, {
+        question : '지구의 날은 몇 월 며칠 인가요? ex) 0927',
+        correct : '0422',
+        hints : 'https://www.korea.kr/news/reporterView.do?newsId=148900968'
+    }, {
+        question : '32개의 글로벌 패션 기업이 체결한 패션 협약에서 0000년까지 온실가스 배출량 제로의 목표를 잡았다 ex) 2022',
+        correct : '2050',
+        hints : 'https://hypebeast.kr/2019/8/g-7-fashion-pact-kering-emmanuel-macron-prada-nike-hermes-burberry-chanel'
+    }, {
+        question : 'ESG에서 G의 의미를 한국어로 입력해주세요',
+        correct : '지배구조',
+        hints : 'https://www.clickesg.co.kr/ui/overview/descriptionEsg.html'
+    }, {
+        question : '새활용 특화시설 중 세계 최대 규모로 평가되는 이곳의 이름을 입력해주세요',
+        correct : '서울새활용플라자',
+        hints : 'https://n.news.naver.com/mnews/article/014/0004933889?sid=102'
+    }, {
+        question : '사람들이 기후변화를 잘 받아들이지 않는 것은 OOO효과 혹은 자기범주화이론이 작용하기 때문이다',
+        correct : '방관자',
+        hints : 'https://ecosophialab.com/%EC%99%9C-%EA%B8%B0%ED%9B%84%EC%9C%84%EA%B8%B0%EB%8A%94-%EC%97%AC%ED%83%9C-%ED%95%B4%EA%B2%B0%EC%9D%B4-%EC%95%88-%EB%90%98%EA%B3%A0-%EC%9E%88%EB%8A%94%EA%B0%80-ccc%EC%9D%98-%EB%B9%84%EB%B0%80-3/'
+    }, {
+        question : '환경문제 해결을 위한 그린피스만의 캠페인 방식 중 세번째는 온라인, 오프라인 시민 OOOOO 프로젝트 진행이다',
+        correct : '액티비스트',
+        hints : 'https://www.greenpeace.org/korea/report/23590/blog-etc-how_we_do/'
+    }, {
+        question : '지구 온난화로 먹을 것이 없어진 순록은 살기 위해 소금기가 많은 OO도 먹는 모습을 보이고 있다',
+        correct : '해초',
+        hints : 'https://www.sciencetimes.co.kr/news/%EB%B6%81%EA%B7%B9-%EC%88%9C%EB%A1%9D-%EC%A7%80%EA%B5%AC-%EC%98%A8%EB%82%9C%ED%99%94-%EC%98%81%ED%96%A5-%EC%8B%9D%EB%8B%A8-%EB%B0%94%EA%BF%94/'
+    }
+  ]
+
   /* user 정보 가져오기 */
   const [user, setUser] = useState([]);
   const router = useRouter();
@@ -23,20 +67,21 @@ export default function quiz(){
     }
   },[])
   
-  /* 데이터 가져올때 쓸겨 */
-  const [data, setData] = useState({});
+  /* 유저 퀴즈 갯수 가져오기, quizzes에서 해당하는 퀴즈 꺼내기 */
+  const [sNum, setSNum] = useState();
+  const [data, setData] = useState();
   useEffect(() => {
     axios.get('http://localhost:3000/api/temp').then(
       res => {
         console.log(res.data);
-        const tmp = {
-          quizNum : res.data[0].quizzes[0].quizNum,
-          question : res.data[0].quizzes[0].question,
-          correct : res.data[0].quizzes[0].correct,
-          hints : res.data[0].quizzes[0].hints
-        }
-        setData(tmp);
+        setSNum(res.data.solvedNum);
     });
+    const tmp = {
+      question : quizzes[sNum].question,
+      correct : quizzes[sNum].correct,
+      hints : quizzes[sNum].hints
+    }
+    setData(tmp);
   },[])
 
   /* 오늘 날짜 */
@@ -58,12 +103,13 @@ export default function quiz(){
   /* 사용자 입력 값 */
   const quizInput = useRef();
 
-  /* 정답 확인 후 css 수정 */
+  /* 정답 확인 */
   function quizConfirm() {
     const quizInput_t = quizInput.current.value.toLowerCase().trim();
     let answerResult = data.correct.toLowerCase().trim();
     if (quizInput_t == answerResult && typeof window !== 'undefined') {
       // 정답 
+      /* 백엔드 서버로 solvedQuiz 갯수 증가시킬 post나 get 필요 */
       document.getElementById('quiz_view_f').classList.remove('block');
       document.getElementById('quiz_view_f').classList.add('hidden');
       document.getElementById('quiz').classList.add('invisible');    // 문제 가리기
@@ -104,7 +150,7 @@ export default function quiz(){
             </Link>
         </div>
         
-        <div className="day-text text-white pt-8 pb-4 text-xl">{month}월 {day}일 {data.quizNum+1}번째 퀴즈</div>
+        <div className="day-text text-white pt-8 pb-4 text-xl">{month}월 {day}일 {sNum+1}번째 퀴즈</div>
         <div className="flex-1 w-full text-center m-auto relative">
           <div className="absolute mt-5 hint-btn">
             <a href={data.hints} className="hint-btn text-xs text-white py-1 px-6 mb-1 bg-green-800 rounded-md">ㅤ힌트 보러가기</a>
