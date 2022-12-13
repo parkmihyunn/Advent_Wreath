ba4194d9bd120f8cac0e6729d27cfc696263354c
ParkMi-Hyun <parkmihyunn@gmail.com>
2022-12-13 23:46:51 +0900

    .
    wish api 사용전 수정 push

--- advent-front/components/reindeerModal.js
+++ advent-front/components/reindeerModal.js
@@ -12,25 +12,26 @@ const ReindeerModal = ({ isVisible, onRClose, usertoken, nickname, solvedNum, de
   return (
     <div>
     <Modal css={{background:"transparent"}} noPadding open={isVisible} preventClose onClose={onRClose} width={300} height={563} animated={false}>
-      <Modal.Header className="flex flex-col items-center text-center w-full mt-36" css={{ position: "absolute", zIndex: "$1"}}>
-        <div className="flex flex-col items-center text-center w-full">
-          <div className="relative w-full mt-10">
-            <div className="relative"><Image src={ deerData.m_body_color} width={93} height={103} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
-            <div className="reindeer1 top-[63%] absolute"><Image src={ deerData.m_body_deco} width={45} height={25} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
-            <div className="reindeer1 top-[28%] absolute"><Image src={ deerData.m_eye} width={37} height={8} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
-            <div className="reindeer1 top-[-13%] absolute"><Image src={ deerData.m_horn} width={67} height={39} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
-            <div className="reindeer1 top-[-2%] absolute"><Image src={ deerData.m_hair} width={23} height={14} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
-          </div>
-          <div className="relative">
-            <div className="pt-10 font-bold text-xl">{solvedNum}번째 순록이 도착했어요 ♥</div>
-            <div className="pt-8 text-sm">{nickname}님만의 특별한 순록이예요.</div>
-            <div className="pt-1 text-sm">방문 앞의 순록도감에서 확인할 수 있어요!</div>
+      <Modal.Header className="flex flex-col items-center text-center w-full" css={{ position: "absolute", zIndex: "$1"}}>
+        <div className="flex flex-col items-center text-center">
+            <div id="inner-white-box" className="relative">
+              <div className="min-w-[200px] min-h-[280px] max-w-[200px] max-h-[500px]"/>
+              <div className="top-[54%] left-[37%] absolute"><Image src={deerData.m_horn} width={64.58} height={37.92} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
+              <div className="top-[64%] left-[30%] absolute"><Image src={deerData.m_body_color} width={91} height={107.68} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
+              <div className="top-[83%] left-[36%] absolute"><Image src={deerData.m_body_deco} width={64.58} height={30.33} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
+              <div className="top-[73%] left-[43.6%] absolute"><Image src={deerData.m_eye} width={36.69} height={9.1} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
+              <div className="top-[61%] left-[37.3%] absolute"><Image src={deerData.m_hair} width={67.52} height={30.33} onError={(e) => (e.currentTarget.src = fallBackSrc)}/></div>
+            </div>
+          <div className="">
+            <div className="pt-10 font-bold text-[16px]">{solvedNum}번째 순록이 도착했어요 ♥</div>
+            <div className="pt-[27px] text-[12px]">{nickname}님만의 특별한 순록이예요.</div>
+            <div className="pt-1 text-[12px]">방문 앞의 순록도감에서 확인할 수 있어요!</div>
           </div>
             <Link href={{
               pathname: '/main',
               query: { value:usertoken }, }} as={`/main?value=${usertoken}`}
             >
-              <button className="mt-16 px-4 py-2 text-white text-base rounded-2xl bg-red-800">맘에 들어요!</button>
+              <button className="mt-[55px] px-4 py-2 text-white text-base rounded-2xl bg-red-800">맘에 들어요!</button>
             </Link>
         </div>
       </Modal.Header>
